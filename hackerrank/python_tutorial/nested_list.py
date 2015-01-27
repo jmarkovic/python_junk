311da707f17ee92f0c780b031202308f5ad381a6
Josip Marković <josip.markovic09@gmail.com>
2015-01-27 14:41:47 +0100

    done nested_list, hackerrank nto working properly yet

--- hackerrank/python_tutorial/nested_list.py
+++ hackerrank/python_tutorial/nested_list.py
@@ -1,13 +1,41 @@
-def get_student():
-    return [input(), float(input())]
+# was not able to test this, hackerrank module stopped working
 
+
+def get_student():
+    name = input("")
+    grade = float(input(""))
+    return [name, grade]
+
+
+def second_lowest(a_list):
+    smallest = 0
+    second_smallest = 0
+    for elem in a_list:
+        if smallest == 0:
+            smallest = elem
+        if elem < smallest:
+            smallest = elem
+
+    for elem in reversed(a_list):
+        if second_smallest == 0:
+            second_smallest = elem
+        if second_smallest > elem > smallest:
+            second_smallest = elem
+
+    return second_smallest
+
+
+# total students to input
 total_inputs = int(input())
 
+# user input
 students = []
 for val in range(total_inputs):
     students.append(get_student())
 
-sorted_students = sorted(students, key=lambda student: student[1])
-sorted_student_names = sorted(student[0] for student in sorted_students if student[1] == sorted_students[0][1])
-for st in sorted_student_names:
-    print(st)
+students = sorted(students, key=lambda student: student[1])
+grades = [student[1] for student in students]
+second_lowest_grade = second_lowest(grades)
+filtered_students = [student for student in students if student[1] == second_lowest_grade]
+for student in filtered_students:
+    print(student[0])
